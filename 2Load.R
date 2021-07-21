be3abdb1479ec8d75e9040c8a3636ced66fa9188
FrederickClasen <frederick.clasen@crick.ac.uk>
2021-07-21 17:47:19 +0100

    added metAnnot rename

--- 2Load.R
+++ 2Load.R
@@ -6,23 +6,27 @@
 
 ######################################################################################################
 
-rawDat = read.csv('RAWDAT.CSV',
+rawDat = read.csv('data/rawDat.csv',
                   sep = ',',
                   header = T)
-sampleAnnot = read.csv('SAMPLEANNOT.CSV',
+sampleAnnot = read.csv('data/sampleAnnot.csv',
                        sep=',',
                        header = T)
-metAnnot = read.csv('METANNOT.CSV',
+metAnnot = read.csv('data/metAnnot.csv',
                     sep=',',
                     header = T)
 
 
 row.names(rawDat) = rawDat[,1]
 rawDat[,1] = NULL
+
 row.names(sampleAnnot) = sampleAnnot[,1]
 sampleAnnot[,1] = NULL
+
 row.names(metAnnot) = metAnnot[,1]
 metAnnot[,1] = NULL
+# the first annotation column is renamed to as it is used later for plotting
+names(metAnnot)[names(metAnnot) == colnames(metAnnot[1])] <- 'Class'
 
 head(rawDat,n=2)
 head(sampleAnnot,n=2)
